a55848e29de76b0aa20332a17b89cbb514712f77
Matheus Porto <mporto1994@gmail.com>
2021-08-10 19:38:54 -0300

    Entrega completa

---
+++ snapCrackle.js
@@ -0,0 +1,27 @@
+function snapCrackle (maxValue){
+    let string = ""   
+    for (let i = 1 ; i <=maxValue ; i++){
+        if (i%5===0){
+            if (i%2!=0){
+                string = string + ", SnapCrackle"
+            }
+            else{
+                string = string + ", Crackle"
+
+            }
+        }
+        else if(i%2 != 0){
+            if (i===1){
+                string = string + "Snap"
+            }
+            else{
+            string = string + ", Snap"
+            }
+        }
+        else{
+            string = string +", "+i
+        }
+
+    }
+    return string
+}
---
+++ snapCracklePrime.js
@@ -0,0 +1,48 @@
+function is_prime(number){
+    let counter = 0
+    for (let i=number ; i>=1 ; i--){
+        if (number%i===0){
+            counter++
+        }
+    }
+    if (counter>2){
+        return false
+    }
+    else {
+        return true
+    }
+}
+
+function snapCracklePrime (maxValue){
+    let string = ""   
+    for (let i = 1; i <=maxValue; i++){
+        if (i%2!=0){
+            if (i===1){
+                string = string + "SnapPrime"
+            }
+            else{
+                string = string + ", Snap"
+                if (i%5===0){
+                    string = string + "Crackle"
+                    if(is_prime(i)){
+                        string = string + "Prime"
+                    }
+                }
+                else if(is_prime(i)){
+                    string = string + "Prime"
+                }
+            }
+        }
+        else if(i%5===0){
+            string = string + ", Crackle"
+        }
+        else if(is_prime(i)){
+            string = string + ", Prime"
+        }
+        else{
+            string = string + ", " + i
+        }
+    }    
+    return string
+}
+document.write(snapCracklePrime(15))
